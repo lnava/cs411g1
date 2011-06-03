4eb968fab6cca1ba08bf838a627df4a45b530221
Lucas Nava <luke.nava@gmail.com>
2011-06-03 13:05:41 -0700

    addd static to tfm decleration

--- linux-2.6.34.7-osurd_encrypt/drivers/block/osurd.c
+++ linux-2.6.34.7-osurd_encrypt/drivers/block/osurd.c
@@ -69,7 +69,7 @@ static int request_mode = RM_SIMPLE;
 module_param(request_mode, int, 0);
 
 /*Variables used for encryption*/
-struct crypto_cipher *tfm;
+static struct crypto_cipher *tfm;
 static int key_len = 8;
 static char crypto_key[8] = "\xdf\xa6\xbf\x4d\xed\x81\xdb\x61";
 module_param_array(crypto_key, byte, &key_len, 0444);
